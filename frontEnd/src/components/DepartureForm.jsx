17e88b08d7670c0c03a46a33e59736a346c68a67
jolaya182 <jolaya182@gmail.com>
2021-05-30 21:32:02 -0400

    Revert "finished orgainzing the flightlist flight cards"
    This reverts commit 2c4a1997531bd48fd325345c39ec9bc3caf3cbe9.

--- frontEnd/src/components/DepartureForm.jsx
+++ frontEnd/src/components/DepartureForm.jsx
@@ -1,22 +1,18 @@
-import {Form, Button } from "react-bootstrap";
-
-const DepartureForm = ({ editDepartureTime }) => {
-  return (
-    <Form>
-      <Form.Group>
-        <Form.Label htmlFor="hour"> hour </Form.Label>
-        <Form.Control id={"hour"} type="text"></Form.Control>
-        <Form.Label htmlFor="minutes"> minutes </Form.Label>
-        <Form.Control id={"minutes"} type="text"></Form.Control>
-        <Form.Label htmlFor="amPm"> Select </Form.Label>
-        <Form.Control id={"amPm"} as="select">
-          <option value={"am"}>am</option>
-          <option value={"pm"}> pm</option>
-        </Form.Control>
-        <Button onClick={editDepartureTime}>change</Button>
-      </Form.Group>
-    </Form>
-  );
-};
+const DepartureForm = ({editDepartureTime})=>{
+    
+    return (
+        <form>
+            <label htmlFor="hour"> hour </label>
+            <input id={"hour"} type="text"  ></input>
+            <label htmlFor="minutes"> minutes </label>
+            <input id={"minutes"} type="text"  ></input>
+            <select id={"amPm"}   >
+                <option value={"am"}>am</option>
+                <option value={"pm"}> pm</option>
+            </select>
+            <button onClick={editDepartureTime}>change</button>
+        
+        </form>);
+}
 
 export default DepartureForm;
